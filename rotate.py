932739e7c821b45f3b59dfc3e40c502be336ff7d
Daniel Schneider <daniel.schneider@hashicorp.com>
2021-11-17 15:45:27 -0500

    way better json

--- rotate.py
+++ rotate.py
@@ -7,6 +7,9 @@ TFC_ORG = os.getenv("TFC_ORG", None)
 TFC_URL = "https://app.terraform.io"
 TFC_WS_FILTER = os.getenv("TFC_WS_FILTER", None)
 
+with open("payload.json", "r") as read_file:
+    payload_tpl = json.load(read_file)
+
 api = TFC(TFC_TOKEN, url=TFC_URL)
 api.set_org(TFC_ORG)
 
@@ -16,10 +19,13 @@ for i in api.workspaces.list()['data']:
     creds_to_rotate = []
     if ws_name == TFC_WS_FILTER:
         state = api.state_versions.get_current(i['id'])
+        payload = payload_tpl
         for r in state['data']['attributes']['resources']:
                 if r['name'] == 'workspace_creds':
                         addr = r['module'].replace('root','module') + '.' + r['type'] + '.' + r['name']
                         creds_to_rotate.append(addr)
-        payload = json.loads('{\"data\":{\"attributes\":{\"message\":\"API Triggered Key Rotation\",\"replace-addrs\":' + str(creds_to_rotate).replace('"','\\"').replace("'",'"') + ',\"auto-apply\":true},\"type\":\"runs\",\"relationships\":{\"workspace\":{\"data\":{\"type\":\"workspaces\",\"id\":\"' + ws_id +'\"}}}}}')
-        print(payload)
+        payload['data']['attributes']['replace-addrs'] = creds_to_rotate
+        payload['data']['relationships']['workspace']['data']['id'] = ws_id
+        print(json.dumps(payload, indent=4, sort_keys=True))
         api.runs.create(payload)
+        
