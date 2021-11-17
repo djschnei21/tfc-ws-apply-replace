06801dfed960554f59b51106926ebd81a795bb43
Daniel Schneider <daniel.schneider@hashicorp.com>
2021-11-17 13:46:57 -0500

    remove debug

--- rotate.py
+++ rotate.py
@@ -6,13 +6,6 @@ TFC_ORG = os.getenv("TFC_ORG", None)
 TFC_WS_FILTER = os.getenv("TFC_WS_FILTER", None)
 TFC_URL = "https://app.terraform.io"
 
-print("----------------")
-print(TFC_TOKEN)
-print(TFC_ORG)
-print(TFC_URL)
-print("----------------")
-
-
 api = TFC(TFC_TOKEN, url=TFC_URL)
 api.set_org(TFC_ORG)
 
