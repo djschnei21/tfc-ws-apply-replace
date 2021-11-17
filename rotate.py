72e0c4b3a643322a6140f975a589b89a569ed720
Daniel Schneider <daniel.schneider@hashicorp.com>
2021-11-17 13:14:12 -0500

    org from env var

--- rotate.py
+++ rotate.py
@@ -2,8 +2,9 @@ import os
 from terrasnek.api import TFC
 
 TFC_TOKEN = os.getenv("TFC_TOKEN", None)
+TFC_ORG = os.getenv("TFC_ORG", None)
 TFC_URL = "https://app.terraform.io"
 
 
 api = TFC(TFC_TOKEN, url=TFC_URL)
-api.set_org("YOUR_ORGANIZATION")
+api.set_org(TFC_ORG)
