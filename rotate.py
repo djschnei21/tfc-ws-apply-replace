69fd8d4d28b7c710dc16c78541927434b66ca483
Daniel Schneider <daniel.schneider@hashicorp.com>
2021-11-17 14:55:19 -0500

    working

--- rotate.py
+++ rotate.py
@@ -1,18 +1,25 @@
 import os
+import json
 from terrasnek.api import TFC
 
 TFC_TOKEN = os.getenv("TFC_TOKEN", None)
 TFC_ORG = os.getenv("TFC_ORG", None)
-TFC_WS_FILTER = os.getenv("TFC_WS_FILTER", None)
 TFC_URL = "https://app.terraform.io"
+TFC_WS_FILTER = os.getenv("TFC_WS_FILTER", None)
 
 api = TFC(TFC_TOKEN, url=TFC_URL)
 api.set_org(TFC_ORG)
 
-found_ws = []
-
 for i in api.workspaces.list()['data']:
-    if i['attributes']['name'].startswith(TFC_WS_FILTER):
-        found_ws.append(i['attributes']['name'])
-
-print(found_ws)
+    ws_name = i['attributes']['name']
+    ws_id = i['id']
+    creds_to_rotate = []
+    if ws_name == TFC_WS_FILTER:
+        state = api.state_versions.get_current(i['id'])
+        for r in state['data']['attributes']['resources']:
+                if r['name'] == 'workspace_creds':
+                        addr = r['module'].replace('root','module') + '.' + r['type'] + '.' + r['name']
+                        creds_to_rotate.append(addr)
+        payload = json.loads('{\"data\":{\"attributes\":{\"message\":\"API Triggered Key Rotation\",\"replace-addrs\":' + str(creds_to_rotate).replace('"','\\"').replace("'",'"') + ',\"auto-apply\":true},\"type\":\"runs\",\"relationships\":{\"workspace\":{\"data\":{\"type\":\"workspaces\",\"id\":\"' + ws_id +'\"}}}}}')
+        print(payload)
+        api.runs.create(payload)
