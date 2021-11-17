2a5074351f991a7bdb9570647b95dbb080c87759
Daniel Schneider <daniel.schneider@hashicorp.com>
2021-11-17 13:39:15 -0500

    debug

--- rotate.py
+++ rotate.py
@@ -5,6 +5,12 @@ TFC_TOKEN = os.getenv("TFC_TOKEN", None)
 TFC_ORG = os.getenv("TFC_ORG", None)
 TFC_URL = "https://app.terraform.io"
 
+print("----------------")
+print(TFC_TOKEN)
+print(TFC_ORG)
+print(TFC_URL)
+print("----------------")
+
 
 api = TFC(TFC_TOKEN, url=TFC_URL)
 api.set_org(TFC_ORG)
